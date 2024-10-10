8d6bb160976787b062fbcc661282c5abb8ef9c2f
stevlabs <steven.arce.arebalo@gmail.com>
2024-10-10 15:44:13 +0200

    Variables en el archivo main.js creado.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* VARIABLES */
+const urlBase = 'https://api.pexels.com/v1';
+const searchInput = document.querySelector("input");
+const btnOrientation = document.querySelector("select");
+const btnRodens = document.querySelector("#btnRodens");
+const btnFelines = document.querySelector("#btnFelines"); 
+const btnReptiles = document.querySelector("#btnReptiles");
+const galery = document.querySelector(".galery");
+const btnLeft = document.querySelector("#btnLeft");
+const btnRight = document.querySelector("#btnRight");
