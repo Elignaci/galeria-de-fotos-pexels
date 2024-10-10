2e01d01c114ad0b67c5b3c8d23239c2e0826e753
nacho <nachogbd@hotmail.com>
2024-10-10 16:53:46 +0200

    funcion conexion creada

--- js/main.js
+++ js/main.js
@@ -3,8 +3,32 @@ const urlBase = 'https://api.pexels.com/v1';
 const searchInput = document.querySelector("input");
 const btnOrientation = document.querySelector("select");
 const btnRodens = document.querySelector("#btnRodens");
-const btnFelines = document.querySelector("#btnFelines"); 
+const btnFelines = document.querySelector("#btnFelines");
 const btnReptiles = document.querySelector("#btnReptiles");
 const galery = document.querySelector(".galery");
 const btnLeft = document.querySelector("#btnLeft");
 const btnRight = document.querySelector("#btnRight");
+
+/* FUNCIONES*/
+const conection = async (action) => {
+
+    try {
+        const resp = await fetch(`${urlBase}/${action}`, {
+            headers: {
+                Authorization: 'Av9UYixj2OarcBJnjGlK75gMv8apHHKAxXd0re2k7WpLfHzDKPLfji17'
+            }
+        });
+        if (resp.ok) {
+            return resp.json()
+        } else {
+            throw Error("Error en la peticion")
+        }
+
+    } catch (error) {
+
+        throw (error.message)
+    }
+}
+
+
+conection()
