4da55f609c328f3e219b792b1cc1ea8d43f48168
stevlabs <steven.arce.arebalo@gmail.com>
2024-10-10 18:16:23 +0200

    Creado dos funciones para pintar las imagenes.

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@ const btnOrientation = document.querySelector("select");
 const btnRodens = document.querySelector("#btnRodens");
 const btnFelines = document.querySelector("#btnFelines");
 const btnReptiles = document.querySelector("#btnReptiles");
-const galery = document.querySelector(".galery");
+const gallery = document.querySelector(".gallery");
 const btnLeft = document.querySelector("#btnLeft");
 const btnRight = document.querySelector("#btnRight");
 
 /* FUNCIONES*/
 const conection = async (action) => {
-
     try {
         const resp = await fetch(`${urlBase}/${action}`, {
             headers: {
@@ -23,12 +22,43 @@ const conection = async (action) => {
         } else {
             throw Error("Error en la peticion")
         }
-
     } catch (error) {
-
         throw (error.message)
     }
 }
 
+const drawInitialImages = async () => {
+    try {
+        const responses = [
+            await conection("photos/51340"),
+            await conection("photos/28826036"),
+            await conection("photos/2078809")
+        ]
+        const buttons = [btnRodens, btnFelines, btnReptiles];
+        responses.forEach((item, index) => {
+            const { src, alt } = item;
+            let img = document.createElement("IMG");
+            img.src = src.original;
+            img.alt = alt;
+            buttons[index].append(img);
+        });
+    } catch (error) {
+        throw (error.message)
+    }
+};
 
-conection()
+const drawImages = async (action) => {
+    try {
+        const resp = await conection(action);
+        resp.photos.forEach(({ src, alt }) => {
+            let img = document.createElement("IMG");
+            img.src = src.original;
+            img.alt = alt;
+            gallery.append(img);
+        });
+    } catch (error) {
+        throw (error.message)
+    }
+};
+
+drawInitialImages();
