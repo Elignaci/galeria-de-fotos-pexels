529ee411dcefb444b6748b2c8f2e6e8738f35646
nacho <nachogbd@hotmail.com>
2024-10-12 16:25:01 +0200

    terminado la paginacion y ajustado el drawImagen con nuevas condiciones del numero de paginas

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ const btnLeft = document.querySelector("#btnLeft");
 const btnRight = document.querySelector("#btnRight");
 const btnCategories = document.querySelector('#btnCategories')
 const pagination = document.querySelector('.pagination')
+
 /* EVENTOS */
 btnCategories.addEventListener('click', (ev) => {
     const button = ev.target.closest('BUTTON');
     if (button) {
         const texTag = button.innerText;
-        //console.log(texTag)
-        drawImages(texTag)
+        drawImages(texTag, 1)
     }
 })
 
@@ -22,18 +22,28 @@ btnImageSearch.addEventListener('click', (ev) => {
     //ev.preventDefault();
     const searchText = searchInput.value;
     //console.log(searchText)
-    drawImages(searchText)
+    drawImages(searchText, 1)
 });
 
 btnOrientation.addEventListener('change', () => {
-    const key = gallery.dataset.key
-    if (key !== '') drawImages(key)
+    const key = gallery.dataset.key;
+    if (key !== '') drawImages(key, 1)
+})
+
+pagination.addEventListener('click', (ev) => {
+    if (ev.target.tagName === 'BUTTON') {
+        const page = parseInt(ev.target.dataset.page);
+        console.log(page)
+        console.log(gallery.dataset.key)
+        drawImages(gallery.dataset.key, page)
+
+    }
 })
 
 /* FUNCIONES*/
-const conection = async (action) => {
+const conection = async (action, page = 1, perPage = 10) => {
     try {
-        const resp = await fetch(`${urlBase}/${action}`, {
+        const resp = await fetch(`${urlBase}/${action}&page=${page}&per_page=${perPage}`, {
             headers: {
                 Authorization: 'Av9UYixj2OarcBJnjGlK75gMv8apHHKAxXd0re2k7WpLfHzDKPLfji17'
             }
@@ -84,13 +94,13 @@ const drawInitialImages = async () => {
     }
 };
 
-const drawImages = async (action) => {
+const drawImages = async (action, page) => {
     try {
         gallery.dataset.key = action
         gallery.innerHTML = ``;
         const fragment = document.createDocumentFragment()
         //console.log(btnOrientation.value)
-        const resp = await conection(`search?query=${action}&orientation=${btnOrientation.value}`);
+        const resp = await conection(`search?query=${action}&orientation=${btnOrientation.value}, page`);
         resp.photos.forEach(({ src, alt }) => {
             let container = document.createElement('DIV')
             let img = document.createElement("IMG");
@@ -100,17 +110,34 @@ const drawImages = async (action) => {
             fragment.append(container);
         });
         gallery.append(fragment)
-        drawPagination(resp,1)
+        drawPagination(resp, page)
     } catch (error) {
         throw (error.message)
     }
 }
-const drawPagination = (resp,currentPage) => {
-    pagination.innerHTML=''
+const drawPagination = (resp, currentPage) => {
+    pagination.innerHTML = ''
     const totalPages = Math.ceil(resp.total_results / resp.per_page)
-    let maxPages=10;
-    for (let i=currentPage; i < currentPage+2; i++){
-        pagination.innerHTML+=`<button>${i}</button>`
+    console.log(totalPages)
+    const maxButtons = 5;
+    let firstPage = (currentPage - 2)
+    let lastPage = (currentPage + 2)
+    if (currentPage <= 3) {
+        if (totalPages <= maxButtons) {
+            lastPage = totalPages;
+        } else {
+            lastPage = maxButtons;
+        }
+        firstPage = 1
+    }
+    if (lastPage > totalPages) {
+        lastPage = totalPages;
+    }
+    for (let i = firstPage; i <= lastPage; i++) {
+        const button = document.createElement('BUTTON')
+        button.innerText = i
+        button.dataset.page = i
+        pagination.append(button);
     }
 
 }
